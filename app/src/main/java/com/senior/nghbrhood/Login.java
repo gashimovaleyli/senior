6d0a16a4d797b0fb855f55183395dea8c9326578
Turann <turan.yucel@ug.bilkent.edu.tr>
2021-02-28 17:33:39 +0300

    Database Connection

--- app/src/main/java/com/senior/nghbrhood/Login.java
+++ app/src/main/java/com/senior/nghbrhood/Login.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
+
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +35,53 @@ public class Login extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
-    public void onClick(View v){
-        Intent intent = null;
-        switch(v.getId()){
-            case R.id.cancelTv:
-                intent = new Intent(this, MainActivity.class);
-                break;
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.loginBtn2:
-                intent = new Intent(this,MainPage.class);
+                final EditText textInputEditTextUserName = (EditText) findViewById(R.id.userNamePt);
+                final EditText textInputEditTextPassword = (EditText) findViewById(R.id.passwordPt);
+
+                username = textInputEditTextUserName.getText().toString();
+                password = textInputEditTextPassword.getText().toString();
+
+                if (!username.equals("") && !password.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.0.195/LoginRegister/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "All the Fields are Required", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.cancelTv:
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent2);
+                finish();
                 break;
         }
-        if (null!=intent) startActivity(intent);
     }
 }
