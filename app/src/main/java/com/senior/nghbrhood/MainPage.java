5c5b79337d8038efd63dfc3bd9488ed36a066a95
Turann <turan.yucel@ug.bilkent.edu.tr>
2021-03-06 00:35:50 +0300

    Change password

--- app/src/main/java/com/senior/nghbrhood/MainPage.java
+++ app/src/main/java/com/senior/nghbrhood/MainPage.java
@@ -50,17 +50,44 @@ public class MainPage extends AppCompatActivity implements androidx.appcompat.wi
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.changePassword:
-                Intent intent = new Intent(this, ChangePassword.class);
-                startActivity(intent);
-                return true;
-            case R.id.logout:
                 SharedPreferences preferences = getSharedPreferences("checkbox", MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putString("remember", "false");
                 editor.apply();
+                Intent intent = new Intent(this, ChangePassword.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout:
+                SharedPreferences preferences2 = getSharedPreferences("checkbox", MODE_PRIVATE);
+                SharedPreferences.Editor editor2 = preferences2.edit();
+                editor2.putString("remember", "false");
+                editor2.apply();
                 Intent intent1 = new Intent(this, MainActivity.class);
                 startActivity(intent1);
                 return true;
+            case R.id.addbills:
+                Intent intent2 = new Intent(this, ChangePassword.class);
+                startActivity(intent2);
+                return true;
+            /*case R.id.showbills:
+                Intent intent3 = new Intent(this, MainActivity.class);
+                startActivity(intent3);
+                return true;*/
+            default:
+                return false;
+        }
+    }
+
+    public void showPopup2(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.bill_popup);
+        popup.show();
+    }
+
+    public boolean onMenuItemClick2(MenuItem item) {
+        switch (item.getItemId()) {
+
             default:
                 return false;
         }
